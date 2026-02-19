feba7bf50e9d2da1342a5718a7b19e2aa7188134
justin <justin@busixstudios.com>
2026-02-19 04:31:24 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/home/HeroSection";
 import PhilosophySection from "@/components/home/PhilosophySection";
 import FeaturedProjects from "@/components/home/FeaturedProjects";
 import StatsSection from "@/components/home/StatsSection";
+import TestimonialsSection from "@/components/home/TestimonialsSection";
 import ContactSection from "@/components/home/ContactSection";
 import FooterSection from "@/components/home/FooterSection";
 
