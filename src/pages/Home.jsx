fab91cb725c8f4f5fa7fa845ae67e79c28834066
justin <justin@busixstudios.com>
2026-02-19 04:33:48 +0000

    Project Modal Enhancements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import HeroSection from "@/components/home/HeroSection";
 import PhilosophySection from "@/components/home/PhilosophySection";
 import FeaturedProjects from "@/components/home/FeaturedProjects";
 import StatsSection from "@/components/home/StatsSection";
-import TestimonialsSection from "@/components/home/TestimonialsSection";
 import ContactSection from "@/components/home/ContactSection";
 import FooterSection from "@/components/home/FooterSection";
 
@@ -14,7 +13,6 @@ export default function Home() {
       <PhilosophySection />
       <FeaturedProjects />
       <StatsSection />
-      <TestimonialsSection />
       <ContactSection />
       <FooterSection />
     </div>
